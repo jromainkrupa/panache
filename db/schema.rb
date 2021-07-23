8047d3b50becd7e949764c82f117c823eaca01d5
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-07-23 14:29:06 +0200

    added columns to users model through migration, phone_number, is_admin, is_club_owner, is_event_admin, first_name, last_name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_122220) do
+ActiveRecord::Schema.define(version: 2021_07_23_122753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2021_07_23_122220) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_number"
+    t.boolean "is_admin"
+    t.boolean "is_club_owner"
+    t.boolean "is_event_admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
