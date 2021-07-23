c71d97b0ee106c8f00707c83aa6b59be871c4982
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-07-23 17:48:43 +0200

    updated routes with clubs and events

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,8 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:home]
+
+  def home
+    @sports = Sport.limit(10)
+    @events = Event.limit(10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  root to: "pages#index"
+  root to: "pages#home"
+
+  resources :events, only: [:index, :show]
 
   devise_for :users
   
-  resources :clubs, only: [:index, :show]
-  
   authenticated :user, lambda {|u| u.is_club_admin?} do
-    resources :clubs, only: %i[new create edit update destroy]
+    resources :clubs, only: %i[new create edit update destroy] do
+      resources :events, only: %i[new create edit update destroy]
+    end
   end
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
