46e24bd72cd0dd96a651cf23047659407c955273
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-07-23 14:33:10 +0200

    created model club

---
+++ db/migrate/20210723123252_create_clubs.rb
@@ -0,0 +1,14 @@
+class CreateClubs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :clubs do |t|
+      t.string :name
+      t.string :address
+      t.string :city
+      t.string :postal_code
+      t.references :user, null: false, foreign_key: true
+      t.string :mail
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_23_123014) do
+ActiveRecord::Schema.define(version: 2021_07_23_123252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clubs", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "city"
+    t.string "postal_code"
+    t.bigint "user_id", null: false
+    t.string "mail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_clubs_on_user_id"
+  end
 
   create_table "sports", force: :cascade do |t|
     t.string "name"
@@ -39,4 +51,5 @@ ActiveRecord::Schema.define(version: 2021_07_23_123014) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "clubs", "users"
 end
