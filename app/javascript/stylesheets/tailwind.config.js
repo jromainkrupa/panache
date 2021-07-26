3ca08aac68ab03b7cd36d5d7d2df5aae49ae5f02
jeanromain KRUPA <jkrupa@hotmail.fr>
2021-07-26 11:47:23 +0200

    added some tailwind components

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,8 +1,70 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: ["production", "staging"].includes(process.env.NODE_ENV),
+    content: [
+      "./**/*.html.erb",
+      "./**/**/*.html.erb",
+      "./**/*.html+phone.erb",
+      "./app/helpers/**/*.rb",
+      "./app/javascript/**/*.js",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#F6F7FF",
+          100: "#EDF0FE",
+          200: "#D2D9FD",
+          300: "#B6C1FB",
+          400: "#8093F9",
+          500: "#4965F6",
+          600: "#425BDD",
+          700: "#2C3D94",
+          800: "#212D6F",
+          900: "#161E4A",
+        },
+        secondary: {
+          50: "#F5FDF9",
+          100: "#ECFAF4",
+          200: "#CFF3E3",
+          300: "#B2ECD2",
+          400: "#78DDB0",
+          500: "#3ECF8E",
+          600: "#38BA80",
+          700: "#257C55",
+          800: "#1C5D40",
+          900: "#133E2B",
+        },
+        tertiary: {
+          50: "#F7F7F8",
+          100: "#EEEEF1",
+          200: "#D5D5DB",
+          300: "#BCBCC5",
+          400: "#898A9A",
+          500: "#57586E",
+          600: "#4E4F63",
+          700: "#343542",
+          800: "#272832",
+          900: "#1A1A21",
+        },
+        danger: {
+          50: "#FEF8F8",
+          100: "#FEF2F2",
+          200: "#FCDEDE",
+          300: "#FACACA",
+          400: "#F7A3A3",
+          500: "#F37B7B",
+          600: "#DB6F6F",
+          700: "#924A4A",
+          800: "#6D3737",
+          900: "#492525",
+        },
+        "code-400": "#fefcf9",
+        "code-600": "#3c455b",
+      }
+    },
   },
   variants: {
     extend: {},
